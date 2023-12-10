050ab699feb932c6431287d2216a113da7b3d445
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-10 11:42:20 -0500

    Add a route so that you can click on individual posts

--- main.py
+++ main.py
@@ -55,12 +55,11 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-# TODO: Add a route so that you can click on individual posts.
+# Add a route so that you can click on individual posts.
 @app.route('/post/<post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-
-    requested_post = "Grab the post from your database"
+    requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
     return render_template("post.html", post=requested_post)
 
 
